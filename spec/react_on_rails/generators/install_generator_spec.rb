44144be1da32a5f118f151a61aceb8afd0f2559b
Recon Vancouver <reconstructions@icloud.com>
2018-01-31 10:21:21 -0800

    869 New Files Merged

--- spec/react_on_rails/generators/install_generator_spec.rb
+++ spec/react_on_rails/generators/install_generator_spec.rb
@@ -1,63 +1,160 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
+
 require_relative "../support/generator_spec_helper"
 require_relative "../support/version_test_helpers"
-describe InstallGenerator, type: :generator do
-  include GeneratorSpec::TestCase
 
+describe InstallGenerator, type: :generator do
   destination File.expand_path("../../dummy-for-generators/", __FILE__)
 
+  let(:default_example_page_name) { "HelloWorld" }
+  let(:default_example_page_path) { "hello_world" }
+  let(:default_example_page_lower_camelcase) { "helloWorld" }
+
+  let(:example_page_name) { default_example_page_name }
+  let(:example_page_path) { default_example_page_path }
+  let(:example_page_lower_camelcase) { default_example_page_lower_camelcase }
+
   context "no args" do
-    before(:all) { run_generator_test_with_args(%w[], package_json: true) }
+    before(:all) { run_generator_test_with_args(%w[]) }
     include_examples "base_generator", application_js: true
     include_examples "no_redux_generator"
   end
 
+  context "--example-page-name" do
+    let(:example_page_name) { "MainPage" }
+    let(:example_page_path) { "main_page" }
+    let(:example_page_lower_camelcase) { "mainPage" }
+
+    context "no redux" do
+      before(:all) do
+        run_generator_test_with_args(%w[--example-page-name MainPage])
+      end
+      include_examples "base_generator", application_js: true
+      include_examples "no_redux_generator"
+    end
+
+    context "with redux" do
+      before(:all) do
+        run_generator_test_with_args(%w[--example-page-name MainPage -R])
+      end
+      include_examples "base_generator", application_js: true
+      include_examples "react_with_redux_generator"
+    end
+  end
+
   context "--redux" do
-    before(:all) { run_generator_test_with_args(%w[--redux], package_json: true) }
+    before(:all) { run_generator_test_with_args(%w[--redux]) }
     include_examples "base_generator", application_js: true
     include_examples "react_with_redux_generator"
   end
 
   context "-R" do
-    before(:all) { run_generator_test_with_args(%w[-R], package_json: true) }
+    before(:all) { run_generator_test_with_args(%w[-R]) }
     include_examples "base_generator", application_js: true
     include_examples "react_with_redux_generator"
   end
 
+  context "--node" do
+    before(:all) { run_generator_test_with_args(%w[--node]) }
+    include_examples "base_generator", application_js: true
+    include_examples "node_generator"
+  end
+
+  context "-N" do
+    before(:all) { run_generator_test_with_args(%w[-N]) }
+    include_examples "base_generator", application_js: true
+    include_examples "node_generator"
+  end
+
   context "without existing application.js or application.js.coffee file" do
-    before(:all) { run_generator_test_with_args([], application_js: false, package_json: true) }
+    before(:all) { run_generator_test_with_args([], application_js: false) }
     include_examples "base_generator", application_js: false
   end
 
   context "with existing application.js or application.js.coffee file" do
-    before(:all) { run_generator_test_with_args([], application_js: true, package_json: true) }
+    before(:all) { run_generator_test_with_args([], application_js: true) }
     include_examples "base_generator", application_js: true
   end
 
   context "with rails_helper" do
-    before(:all) { run_generator_test_with_args([], spec: true, package_json: true) }
+    before(:all) { run_generator_test_with_args([], spec: true) }
     it "adds ReactOnRails::TestHelper.configure_rspec_to_compile_assets(config)" do
       expected = ReactOnRails::Generators::BaseGenerator::CONFIGURE_RSPEC_TO_COMPILE_ASSETS
       assert_file("spec/rails_helper.rb") { |contents| assert_match(expected, contents) }
     end
   end
 
+  context "with missing files to trigger errors" do
+    specify "GeneratorMessages has the missing file error" do
+      run_generator_test_with_args([], gitignore: false)
+      expected = <<-MSG.strip_heredoc
+        .gitignore was not found.
+        Please add the following content to your .gitignore file:
+        # React on Rails
+        npm-debug.log*
+        node_modules
+
+        # Generated js bundles
+        /public/webpack/*
+
+        MSG
+      expect(GeneratorMessages.output)
+        .to include(GeneratorMessages.format_error(expected))
+    end
+  end
+
   context "with helpful message" do
     let(:expected) do
-      GeneratorMessages.format_info(ReactOnRails::Generators::BaseGenerator.helpful_message)
+      <<-MSG.strip_heredoc
+
+        What to do next:
+
+          - Include your webpack assets to your application layout.
+
+            <%= javascript_pack_tag 'main' %>
+
+          - Ensure your bundle and yarn installs of dependencies are up to date.
+
+              bundle && yarn
+
+          - Run the foreman command to start the rails server and run webpack in watch mode.
+
+              foreman start -f Procfile.dev
+
+          - Visit http://localhost:3000/hello_world and see your React On Rails app running!
+      MSG
     end
 
     specify "base generator contains a helpful message" do
-      run_generator_test_with_args(%w[], package_json: true)
-      # GeneratorMessages.output is an array with the git error being the first one
-      expect(GeneratorMessages.output).to include(expected)
+      run_generator_test_with_args(%w[])
+      expect(GeneratorMessages.output)
+        .to include(GeneratorMessages.format_info(expected))
     end
 
     specify "react with redux generator contains a helpful message" do
-      run_generator_test_with_args(%w[--redux], package_json: true)
-      # GeneratorMessages.output is an array with the git error being the first one
-      expect(GeneratorMessages.output).to include(expected)
+      run_generator_test_with_args(%w[--redux])
+      expected = <<-MSG.strip_heredoc
+
+        What to do next:
+
+          - Include your webpack assets to your application layout.
+
+            <%= javascript_pack_tag 'main' %>
+
+          - Ensure your bundle and yarn installs of dependencies are up to date.
+
+              bundle && yarn
+
+          - Run the foreman command to start the rails server and run webpack in watch mode.
+
+              foreman start -f Procfile.dev
+
+          - Visit http://localhost:3000/hello_world and see your React On Rails app running!
+      MSG
+      expect(GeneratorMessages.output)
+        .to include(GeneratorMessages.format_info(expected))
     end
   end
 
@@ -123,5 +220,54 @@ describe InstallGenerator, type: :generator do
       allow(@install_generator).to receive(:`).with("where yarn").and_return("")
       expect(@install_generator.send(:missing_yarn?)).to eq true
     end
+  end
+
+  context "when package.json already exists" do
+    before(:all) { @install_generator = InstallGenerator.new }
+
+    specify "it adds the script section if missing" do
+      data = <<-DATA.strip_heredoc
+        {
+          "dependencies": {}
+        }
+      DATA
+
+      run_generator_test_with_args(%w[]) do
+        simulate_existing_file("package.json", data)
+      end
+
+      assert_file("package.json") do |contents|
+        parsed = JSON.parse(contents)
+        assert(parsed["scripts"]["postinstall"])
+      end
+    end
+
+    specify "it adds the postinstall script to the script section" do
+      data = <<-DATA.strip_heredoc
+        {
+          "scripts": {
+            "foo": "bar"
+          },
+          "dependencies": {
+          }
+        }
+      DATA
+
+      run_generator_test_with_args(%w[]) do
+        simulate_existing_file("package.json", data)
+      end
+
+      assert_file("package.json") do |contents|
+        parsed = JSON.parse(contents)
+        assert(parsed["scripts"]["postinstall"])
+      end
+    end
+  end
+
+  def convert_example_page_name(string)
+    string.gsub(default_example_page_name, example_page_name)
+          .gsub(default_example_page_path, example_page_path)
+          .gsub(default_example_page_lower_camelcase,
+                example_page_lower_camelcase)
   end
 end
